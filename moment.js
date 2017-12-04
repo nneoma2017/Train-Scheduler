f43f1a88afa7ea06e17e5803f31d71bc9d9f235f
Nneoma Akubuilo <nneomaakubuilo@tests-mbp-3.attlocal.net>
2017-12-04 06:49:59 -0800

    updated

--- moment.js
+++ moment.js
@@ -1,104 +1,77 @@
-  var config = {
-    apiKey: "AIzaSyATM10OO1zzBbk0TU9v-JCWbL_956_vWMg",
-    authDomain: "my-awesome-project-b91a1.firebaseapp.com",
-    databaseURL: "https://my-awesome-project-b91a1.firebaseio.com",
-    projectId: "my-awesome-project-b91a1",
-    storageBucket: "my-awesome-project-b91a1.appspot.com",
-    messagingSenderId: "466471583524"
+
+var config = {
+    //insert firebase keys
   };
-  firebase.initializeApp(config);
-  
 
-  //store database in new variable 
+firebase.initializeApp(config);
 
-   var database = firebase.database();
+var trainData = firebase.database();
 
+$("#add-train-btn").on("click", function() {
 
-  //add button for adding new train
-  $("#add-train-btn").on("click", function(event) {
-     event.preventDefault();
-     console.log("#add-train-btn");
-     
+  // Grabs user input
+  var trainName = $("#train-name-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var firstTrain = $("#first-train-input").val().trim();
+  var frequency = $("#frequency-input").val().trim();
 
-
-     //user input is stored in new variable 
-
-     var trainName = $("#train-name-input").val().trim();
-     console.log(trainName);
-  
-
-     var trainDes =  $("#destination").val().trim();
-
-     var trainTime = $("#first-train-time").val().trim();
-
-     var trainFrequency = $("#frequency").val().trim();
-
-    //create tempoary object
-
-    var newTrain = {
+  var newTrain = {
 
     name: trainName,
-
-    destination: trainDes,
-
-    time: trainTime,
-
-    frequency: trainFrequency,
-
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
   };
 
-  console.log(newTrain);
+  trainData.ref().push(newTrain);
 
-   database.ref().push(newTrain);
-
-   console.log(newTrain.name);
-   console.log(newTrain.destination);
-   console.log(newTrain.time);
-   console.log(newTrain.frequency);
-
-   alert("Train Succesfully Added");
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTrain);
+  console.log(newTrain.frequency);
 
 
-      $("#train-name-input").text("");
+  alert("Train successfully added");
 
-     $("#destination").text("");
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
 
-     $("#first-train-time").text("");
-
-     $("#frequency").text("");
-
-
-     return false;
- });
-
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-  console.log(childSnapshot.val());
-
-  var trainName = childSnapshot.val().name;
-  var trainDes = childSnapshot.val().destination;
-  var trainTime = childSnapshot.val().time;
-  var trainFrequency = childSnapshot.val().frequency;
-
-   console.log(trainName);
-   console.log(trainDes);
-   console.log(trainTime);
-   console.log(trainFrequency);
-
-   //get current train time amd frequency. use moments to convert to proper librbary
-
-   var current = moment();
-   varc currentTrainTime = moment.unix(trainTime).format("MM/DD/YY");
-  
-  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDes+ "</td><td>" +
-  trainTime + "</td><td>" + trainFrequency);
+  return false;
 });
 
+trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
+      console.log(childSnapshot.val());
 
+      // Store everything into a variable.
+      var tName = childSnapshot.val().name;
+      var tDestination = childSnapshot.val().destination;
+      var tFrequency = childSnapshot.val().frequency;
+      var tFirstTrain = childSnapshot.val().firstTrain;
 
+      var timeArr = tFirstTrain.split(":");
+      var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+      var maxMoment = moment.max(moment(), trainTime);
+      var tMinutes;
+      var tArrival;
+      
+      if (maxMoment === trainTime) {
+        tArrival = trainTime.format("hh:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
+      } else {
 
+        
+        var differenceTimes = moment().diff(trainTime, "minutes");
+        var tRemainder = differenceTimes % tFrequency;
+        tMinutes = tFrequency - tRemainder;
+        tArrival = moment().add(tMinutes, "m").format("hh:mm A");
+      }
+        console.log("tMinutes:", tMinutes);
+        console.log("tArrival:", tArrival);
 
-
-
+        $("#train-table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" +
+          tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
+      });
 
